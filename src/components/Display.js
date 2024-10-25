9aa60d869b54b95e1b7f5c4b417ef7adbabc429d
MUHAMMEDSHAMMAS361 <mhdshammas011@gmail.com>
2024-10-25 20:02:57 +0530

    1st commit

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,40 +1,36 @@
 import React, { useEffect, useState } from 'react';
-import '../App.css'
+import '../App.css';
 
 const Fetch = () => {
     const [data, setData] = useState([]); 
-    const [Body, setBody] = useState('');
+    const [Body, setBody] = useState(null);
 
     useEffect(() => {
-
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json()) 
             .then(data => setData(data)) 
             .catch(error => console.error("Error fetching posts:", error)); 
     }, []); 
 
-    const handleBody = (body) => {
-        setBody(body); 
+    const handleBody = (id) => {
+        setBody(Body === id ? null : id); 
     };
 
     return (
         <div>
-            <h1> Title</h1>
-            <ul>
-                {data.map(val => (
-                    <li key={val.id} onClick={() => handleBody(val.body)}>
-                        {val.title} 
-                    </li>
-                
+            <h1>Title</h1>
+            <div>
+                {data.map((val) => (
+                    <div key={val.id} className="card" onClick={() => handleBody(val.id)}>
+                        <p><b>TITLE:</b> {val.title}</p>
+                        {Body === val.id && ( 
+                            <div>
+                                <p className='displayBody'><b>BODY:</b> {val.body}</p>
+                            </div>
+                        )}
+                    </div>
                 ))}
-                      {Body && (
-                        <div>
-                            <h2>Body</h2>
-                            <p className='displayBody'>{Body}</p>
-                        </div>
-                    )}
-            </ul>
-          
+            </div>
         </div>
     );
 };
